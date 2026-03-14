cdcb6b33e50da35af7d47a72eeea8ee2cfabb1cd
cushonz <zachary.cushon@gmail.com>
2026-03-14 04:30:59 -0700

    Search cards mapping

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,7 +1,27 @@
 import Header from "../components/Header.jsx";
 import SearchCard from "../components/SearchCard.jsx";
+import { useState } from "react";
 
 function Search() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [SearchResults, setSearchResults] = useState([]);
+
+  async function handleSearch() {
+    try {
+      const response = await fetch("http://localhost:3001/search-itunes", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ term: searchTerm }),
+      });
+
+      const data = await response.json();
+      setSearchResults(data.results);
+    } catch (error) {
+      console.error("Error searching iTunes:", error);
+      // show user an error message
+    }
+  }
+
   return (
     <div className="bg-zinc-950 min-h-screen min-w-screen">
       <Header />
@@ -10,17 +30,23 @@ function Search() {
           type="text"
           placeholder="Search for songs..."
           className="bg-zinc-800 text-white placeholder:text-zinc-500 border border-zinc-700 focus:outline-none focus:border-green-500 rounded-md"
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <button className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-md ml-2">
+        <button
+          className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-md ml-2"
+          onClick={handleSearch}
+        >
           Search
         </button>
       </div>
-      <SearchCard
-        name="Creep"
-        artist="Radiohead"
-        Album="Pablo Honey"
-        albumArt="https://placehold.co/100x100?text=Art"
-      />
+      {SearchResults.map((result, index) => (
+        <SearchCard
+          key={index}
+          name={result.name}
+          artist={result.artist}
+          albumArt={result.albumArt}
+        />
+      ))}
     </div>
   );
 }
